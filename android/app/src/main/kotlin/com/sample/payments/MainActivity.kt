33de1f5332ed07471995a78313e253e4030b3146
Gokul <gokulakrishnan3099@gmail.com>
2025-10-09 19:05:28 +0530

    Initial commit for android

--- android/app/src/main/kotlin/com/sample/payments/MainActivity.kt
+++ android/app/src/main/kotlin/com/sample/payments/MainActivity.kt
@@ -1,5 +1,30 @@
 package com.sample.payments
 
+import com.sample.payments.MainActivity.Companion.CHANNEL
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+
+class MainActivity: FlutterActivity(){
+    val mainController: MainController by lazy {
+        MainController(this@MainActivity)
+    }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+            .setMethodCallHandler { call: MethodCall, result: MethodChannel.Result ->
+                if (call.method == "getProducts") {
+                    mainController.getProducts(result)
+                } else {
+                    result.notImplemented()
+                }
+            }
+    }
+
+    companion object {
+        const val CHANNEL: String = "com.example.myapp/native"
+    }
+}
